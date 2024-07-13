a2c6c77687ed4f2e11aca7e565c0ac1abe4a35d9
clarkzjw <i@jinwei.me>
2024-07-12 23:28:09 -0700

    add compression

--- cmd/lens/main.go
+++ cmd/lens/main.go
@@ -312,26 +312,29 @@ func icmp_ping(target string, interval float64) {
 	defer cancel()
 
 	today := checkDirectory()
+	filename := fmt.Sprintf("ping-%s-%s-%s-%s-%s.txt", PoP, target, INTERVAL, DURATION, getTimeString())
+	filename_full := path.Join("data", today, filename)
 
-	cmd := exec.CommandContext(ctx, "ping", "-D", "-c", fmt.Sprintf("%d", COUNT), "-i", fmt.Sprintf("%.2f", interval), "-I", IFACE, target)
+	go func(ctx context.Context) {
+		cmd := exec.CommandContext(ctx, "ping", "-D", "-c", fmt.Sprintf("%d", COUNT), "-i", fmt.Sprintf("%.2f", interval), "-I", IFACE, target)
 
-	filename := path.Join("data", today, fmt.Sprintf("ping-%s-%s-%s-%s-%s.txt", PoP, target, INTERVAL, DURATION, getTimeString()))
+		f, err := os.Create(filename_full)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer f.Close()
 
-	f, err := os.Create(filename)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer f.Close()
+		mw := io.MultiWriter(f)
 
-	mw := io.MultiWriter(f)
+		cmd.Stdout = mw
+		cmd.Stderr = mw
 
-	cmd.Stdout = mw
-	cmd.Stderr = mw
+		if err := cmd.Run(); err != nil {
+			log.Println(err)
+		}
+	}(ctx)
 
-	if err := cmd.Run(); err != nil {
-		log.Panic(err)
-	}
-
+	<-ctx.Done()
 	if err := compress(path.Join(DATA_DIR, today), filename); err != nil {
 		log.Println(err)
 	}
@@ -343,21 +346,25 @@ func irtt_ping() {
 
 	today := checkDirectory()
 
-	filename := path.Join("data", today, fmt.Sprintf("irtt-%s-%s-%s-%s.json", PoP, INTERVAL, DURATION, getTimeString()))
+	filename := fmt.Sprintf("irtt-%s-%s-%s-%s.json", PoP, INTERVAL, DURATION, getTimeString())
+	filename_full := path.Join("data", today, filename)
 
-	var local string
-	if IPVersion == 6 {
-		local = fmt.Sprintf("--local=[%s]", external_ip6)
-	} else {
-		local = fmt.Sprintf("--local=%s", LOCAL_IP)
-	}
+	go func(ctx context.Context) {
+		var local string
+		if IPVersion == 6 {
+			local = fmt.Sprintf("--local=[%s]", external_ip6)
+		} else {
+			local = fmt.Sprintf("--local=%s", LOCAL_IP)
+		}
 
-	cmd := exec.CommandContext(ctx, "irtt", "client", fmt.Sprintf("-%d", IPVersion), "-Q", "-i", INTERVAL, "-d", DURATION, local, IRTT_HOST_PORT, "-o", filename)
+		cmd := exec.CommandContext(ctx, "irtt", "client", fmt.Sprintf("-%d", IPVersion), "-Q", "-i", INTERVAL, "-d", DURATION, local, IRTT_HOST_PORT, "-o", filename_full)
 
-	if err := cmd.Run(); err != nil {
-		log.Panic(err)
-	}
+		if err := cmd.Run(); err != nil {
+			log.Println(err)
+		}
+	}(ctx)
 
+	<-ctx.Done()
 	if err := compress(path.Join(DATA_DIR, today), filename); err != nil {
 		log.Println(err)
 	}
